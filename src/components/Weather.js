c5f89034815cc56ec24cce7d813f95356fde028b
vknanavati <vimin.nanavati@gmail.com>
2024-08-07 14:07:11 -0400

    added weather app.

---
+++ src/components/Weather.js
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import { Container, Grid, Typography, TextField, Button, Box } from '@mui/material';
+import {makeStyles} from '@mui/styles';
+
+console.log(process.env)
+
+const useStyles = makeStyles({
+  weatherData: {
+    color:"#6B117D",
+    fontSize: [24, "!important"]
+  }
+})
+
+export function Weather() {
+    const classes = useStyles();
+
+    const [city, setCity] = useState("");
+    const [weather, setWeather] = useState("");
+
+    const handleCity = e => {
+      setCity(e.target.value)
+    }
+
+    const handleClick = (event) => {
+      event.preventDefault()
+      console.log("Searching city: ", city, process.env.REACT_APP_API_KEY)
+
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+          setWeather(data)
+        })
+    }
+    return (
+    <Container>
+    <Grid
+      container
+      justifyContent={"center"}
+      alignItems={"center"}
+      direction={"column"}
+    >
+      <Typography
+        variant="h4"
+        sx={{marginTop: 7, marginBottom: 4, color: "#953ef7"}}
+      >
+        Current Weather
+      </Typography>
+      <form>
+        <Grid
+          container
+          spacing={2}
+          alignItems={"center"}
+          sx={{marginTop: 1}}
+        >
+          <Grid item>
+            <TextField
+              sx={{
+                width: 250,
+                "& .MuiInputBase-root":{
+                  height: 50
+                }
+              }}
+              id="city"
+              type="text"
+              placeholder="enter city"
+              value={city}
+              onChange={handleCity}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              sx={{backgroundColor: "#953ef7",
+              "&:hover": {
+                backgroundColor: "#953ef7",
+              }
+              }}
+              onClick={e => handleClick(e)}
+            >Search
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      {weather && (
+        <Box
+          textAlign={"center"}
+          padding={2}
+          height={250}
+          width={250}
+          borderRadius={5}
+          marginTop={6}
+          backgroundColor={"#d6b4fc"}
+          fontSize={20}
+          lineHeight={1}
+          >
+        <Typography className={classes.weatherData} sx={{ marginTop: 1}}>{weather.name}</Typography>
+        <img alt="weather-image" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} />
+        <Typography className={classes.weatherData} sx={{ marginTop:3 }}>{weather.weather[0].description}</Typography>
+        <Typography className={classes.weatherData}>{weather.main.temp.toFixed(1)} °F</Typography>
+      </Box>
+      )}
+    </Grid>
+  </Container>
+)
+}
